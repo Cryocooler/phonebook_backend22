c83363a816b14c984a54afdd192c4b4ab108d094
Cryocooler <sahin.dursun@icloud.com>
2022-05-09 19:49:35 +0300

    deploy fixes

--- models/person.js
+++ models/person.js
@@ -1,35 +1,31 @@
+const mongoose = require("mongoose");
+const validator = require("mongoose-unique-validator");
 
-   
-const mongoose = require('mongoose')
-const validator = require('mongoose-unique-validator')
+const url = process.env.MONGODB_URI;
+console.log("connecting to", url);
 
-const url = process.env.MONGODB_URI
-console.log('connecting to', url)
-
-mongoose.connect(url)
-    .then(res => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+mongoose
+  .connect(url)
+  .then((res) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 const personSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true, minlength: 3},
-    number: {type: String, required: true, minlength:  8}
-    
-})
+  name: { type: String, required: true, unique: true, minlength: 3 },
+  number: { type: String, required: true, minlength: 8 },
+});
 
-personSchema.plugin(validator)
+personSchema.plugin(validator);
 
-personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-      }
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-})
-
-
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model("Person", personSchema);
